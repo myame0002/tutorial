51f7af327040bf62d35361452a65d7767a8d1154
myame0002 <picturepicture773@gmail.com>
2026-04-03 03:36:54 +0900

    fix rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,17 +1,11 @@
-import { getCollection } from 'astro:content';
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
-  const posts = await getCollection('blog');
   return rss({
     title: 'Astro学習者 | ブログ',
     description: 'Astroを学ぶ旅',
     site: context.site,
-    items: posts.map((post) => ({
-      title: post.data.title,
-      description: post.data.description,
-      pubDate: post.data.pubDate,
-      link: `/posts/${post.id}/`,
-    })),
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>ja-jp</language>`,
   });
 }
